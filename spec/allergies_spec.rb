3e8630f32d346ce9e29c76ed36a6627fd6678e5c
drewparker <drewjparker91@gmail.com>
2020-09-24 13:27:22 -0700

    create second test returning peanuts for input of 2 Co-Authored-By: Vanessa Stewart <vstewart1213@gmail.com>

--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -6,4 +6,10 @@ describe('AllergyScore#get_allergies') do
     allergies = AllergyScore.new("1")
     expect(allergies.get_allergies).to(eq(["eggs"]))
   end
+
+  it('returns peanuts if input is 2') do
+    allergies = AllergyScore.new("2")
+    expect(allergies.get_allergies).to(eq(["peanuts"]))
+  end
+
 end
